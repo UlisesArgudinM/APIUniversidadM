a2158fb374068fb2573f91709183980e1f274aba
UlisesArgudinM <masterinfame@hotmail.com>
2022-05-03 19:34:04 -0500

    se agrega el proyecto

---
+++ src/main/java/com/ibm/academia/restapi/universidad/repositorios/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package com.ibm.academia.restapi.universidad.repositorios;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepository extends PersonaRepository 
+{
+	
+}
---
+++ src/main/java/com/ibm/academia/restapi/universidad/repositorios/ProfesorRepository.java
@@ -0,0 +1,8 @@
+package com.ibm.academia.restapi.universidad.repositorios;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfesorRepository extends PersonaRepository
+{
+}
